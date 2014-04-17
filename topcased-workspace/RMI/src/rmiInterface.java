4109bb0093c727688c49ad92b67d8109e9f054d0
sabrallte <truesepp@gmail.com>
2014-04-17 09:24:59 +0200

    Verzeichniss-struktur vereinfacht
    aktives Arbeitsverzeichniss, SourceFiles:
    \topcased-workspace\SE-Beleg-Java\src
    eure noch nicht implementierten Module: \topcased-workspace\LernModule

--- topcased-workspace/RMI/src/rmiInterface.java
+++
@@ -1,5 +0,0 @@
-
-//interface muss auf client und host seite als .class vorliegen
-public interface rmiInterface extends java.rmi.Remote{
-	public String toUpper(String text) throws java.rmi.RemoteException;
-}
